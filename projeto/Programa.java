70dc8818c73ea31ddcaef4124b46b8e971f35b91
Luna Mendes <lkmnds@gmail.com>
2017-11-09 16:25:06 -0300

    add Programa.batalhar

--- projeto/Programa.java
+++ projeto/Programa.java
@@ -1,12 +1,31 @@
 package projeto;
 
 public class Programa {
+	/**
+	 * Make a battle between the player and a wild pokémon
+	 * 
+	 * @param player
+	 * @param selvagem
+	 * @return boolean
+	 */
+	public static boolean batalhar(Player player, Pokemon selvagem) {
+		int i = 1;
+		Pokemon maisForte = player.getPokemon(0);
+		while(true) {
+			Pokemon p = player.getPokemon(i);
+			if(p == null) break;
 
-
-
-	public static boolean batalhar(Player player, BancoPokemon selvagem) {
-		// TODO Auto-generated method stub
-		return false;
+			if(p.getNivel() > maisForte.getNivel()) {
+				maisForte = p;
+			}
+			
+			i++;
+		}
+			
+		return maisForte.getNivel() >= selvagem.getNivel();
 	}
-
+	
+	public static void main(String[] args) {
+		return;
+	}
 }
